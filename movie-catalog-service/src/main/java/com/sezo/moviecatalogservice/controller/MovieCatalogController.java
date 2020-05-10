b2b96e8a197371823e65ea473e8bd6456f981bcb
sezayirdagtekin <sezayir@hotmail.com>
2020-05-10 18:51:24 +0300

    oldwaye:use RestTemplate for cominication

--- movie-catalog-service/src/main/java/com/sezo/moviecatalogservice/controller/MovieCatalogController.java
+++ movie-catalog-service/src/main/java/com/sezo/moviecatalogservice/controller/MovieCatalogController.java
@@ -1,22 +1,37 @@
 package com.sezo.moviecatalogservice.controller;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
 import com.sezo.moviecatalogservice.model.Catalog;
+import com.sezo.moviecatalogservice.model.Movie;
+import com.sezo.moviecatalogservice.model.MovieRating;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogController {
+	
+	@Autowired
+	RestTemplate restTemplate;
+
+	private static final String SERVICE_URL = "http://localhost:8082/movies/";
 
 	@RequestMapping("/{id}")
-	public List<Catalog> getCatalog(@PathVariable("id") int id) {
+	public List<Catalog> getCatalog(@PathVariable("id") String id) {
 
-		return Collections.singletonList(new Catalog("Transformers", "Transformers description", id));
+		List<MovieRating> ratings = Arrays.asList(new MovieRating(4, 80), new MovieRating(3, 90));
+
+		return ratings.stream().map(r -> {
+			Movie movie = restTemplate.getForObject(SERVICE_URL + r.getMovieId(), Movie.class);
+			return new Catalog(movie.getName(), "Description", r.getRating());
+		}).collect(Collectors.toList());
 
 	}
 
---
+++ movie-catalog-service/src/main/java/com/sezo/moviecatalogservice/model/Movie.java
@@ -0,0 +1,34 @@
+package com.sezo.moviecatalogservice.model;
+
+public class Movie {
+
+	private int id;
+	private String name;
+
+	public Movie() {
+		super();
+	}
+
+	public Movie(int id, String name) {
+
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
